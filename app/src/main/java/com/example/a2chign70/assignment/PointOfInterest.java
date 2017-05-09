d1baf669e8e9d6907c74eae979739e9fdee04296
Nicola Chigumbura <Nicola Chigumbura>
2017-05-09 15:44:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/a2chign70/assignment/PointOfInterest.java
@@ -0,0 +1,38 @@
+package com.example.a2chign70.assignment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+public class PointOfInterest extends Activity
+{
+
+    MapView mv;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+
+        super.onCreate(savedInstanceState);
+
+        // This line sets the user agent, a requirement to download OSM maps
+        Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        setContentView(R.layout.activity_point_of_interest);
+
+        mv = (MapView)findViewById(R.id.map1);
+
+        mv.setBuiltInZoomControls(true);
+        mv.getController().setZoom(14);
+        mv.getController().setCenter(new GeoPoint(51.05,-0.72));
+    }
+}
